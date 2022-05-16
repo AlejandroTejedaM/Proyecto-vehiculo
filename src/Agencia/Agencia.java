c838c477f8a85c391724b952223756d7bbdbc2f4
AlejandroTejedaM <alex2227@hotmail.com>
2022-05-16 11:57:39 -0500

    Se declararon variables

--- src/Agencia/Agencia.java
+++ src/Agencia/Agencia.java
@@ -8,6 +8,7 @@ public class Agencia {
 
     public static void main(String[] args) {
         Motocicleta motocicleta;
+        AutoElectrico autoElectrico;
         Vehiculo vehiculo;
         CRUD crud = new CRUD();
         int indice = 0;
